e5bc0a74bb55f41ef66e14cf34888cf9d95e91fc
douchebag1108 <cjy653@naver.com>
2022-03-08 14:34:30 +0900

    duplicate fail, add og profile.

--- miniProject/app.py
+++ miniProject/app.py
@@ -22,8 +22,8 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -34,21 +34,6 @@ def home():
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
-
-
-#     # 프로필, 게시글 서버
-# @app.route('/user/<username>')
-# def user(username):
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-#
-#         user_info = db.users.find_one({"username": username}, {"_id": False})
-#         return render_template('user.html', user_info=user_info, status=status)
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
@@ -75,13 +60,13 @@ def sign_in():
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
-    nick_receive = request.form['nick_give']
+    # nick_receive = request.form['nick_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
         "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
-        "profile_name": nick_receive,                               # 프로필 이름 기본값은 닉네임----------원래는 id
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 닉네임----------원래는 id
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -93,13 +78,13 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-# 닉네임 중복확인 서버
-@app.route('/sign_up/check_nick', methods=['POST'])
-def check_nick():
-    nick_receive = request.form['nick_give']
-    exists = bool(db.users.find_one({"nick": nick_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
+# # 닉네임 중복확인 서버
+# @app.route('/sign_up/check_nick', methods=['POST'])
+# def check_nick():
+#     nick_receive = request.form['nick_give']
+#     exists = bool(db.users.find_one({"nick": nick_receive}))
+#     return jsonify({'result': 'success', 'exists': exists})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5100, debug=True)
