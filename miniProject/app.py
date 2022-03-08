b6381526381fc91d00a42d55f8048c54597e43be
douchebag1108 <cjy653@naver.com>
2022-03-08 16:43:42 +0900

    duplicate fail, add og profile.

--- miniProject/app.py
+++ miniProject/app.py
@@ -13,7 +13,7 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('mongodb+srv://dushbag:<dushbag>@cluster0.jdbsd.mongodb.net/cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://dushbag:dushbag@cluster0.jdbsd.mongodb.net/cluster0?retryWrites=true&w=majority')
 db = client.dbsparta_plus_week4
 
 # 토큰
@@ -30,16 +30,17 @@ def home():
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route('/login')
+@app.route('/api/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-@app.route('/sign_in', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def sign_in():
     # 로그인 서버
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
+    nick_receive = request.form['nickname_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     result = db.users.find_one({'username': username_receive, 'password': pw_hash})
@@ -52,38 +53,37 @@ def sign_in():
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
         return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 # 회원가입 서버
-@app.route('/sign_up/save', methods=['POST'])
+@app.route('/api/register', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
-    # nick_receive = request.form['nick_give']
+    nick_receive = request.form['nickname_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
         "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 닉네임----------원래는 id
+        "profile_name": nick_receive,                           # 프로필 이름 기본값은 닉네임----------원래는 id
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
 # 아이디 중복확인 서버
-@app.route('/sign_up/check_dup', methods=['POST'])
+@app.route('/api/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-# # 닉네임 중복확인 서버
-# @app.route('/sign_up/check_nick', methods=['POST'])
-# def check_nick():
-#     nick_receive = request.form['nick_give']
-#     exists = bool(db.users.find_one({"nick": nick_receive}))
-#     return jsonify({'result': 'success', 'exists': exists})
+# 닉네임 중복확인 서버
+@app.route('/api/check_nick', methods=['POST'])
+def check_nick():
+    nick_receive = request.form['nickname_give']
+    exists = bool(db.users.find_one({"nick": nick_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 
 if __name__ == '__main__':
